ea87461010ecb3612efa41737a1c014925f6ca9e
RatajX <58147863+RatajXX1@users.noreply.github.com>
2023-06-02 01:16:49 +0200

    sync

---
+++ src/ui/FilesSideBar/FilesSideBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./FilesSideBar.scss";
+
+class FilesSideBar extends React.Component {
+
+    render(): React.ReactNode {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default FilesSideBar;
